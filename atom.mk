2679eaa73f9fc7ffba7c69c9ce2327b299248c1c
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-12-10 17:38:50 +0100

    fullscreen button + base drawing of controls

--- atom.mk
+++ atom.mk
@@ -7,6 +7,7 @@ LOCAL_DESCRIPTION := Javascript client controlling the vehicle
 LOCAL_CATEGORY_PATH := controllers
 
 LOCAL_COPY_FILES := \
+	img/fullscreen.png:var/www/img/ \
 	js/client.js:var/www/js/ \
 	js/phaser.min.js:var/www/js/ \
 	css/style.css:var/www/css/ \
--- js/client.js
+++ js/client.js
@@ -1,10 +1,51 @@
-  var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
-    preload: preload, create: create, update: update
-  });
-  function preload() {
-    game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-    game.stage.backgroundColor = '#eee';
-  }
-  function create() {}
-  function update() {}
+/* modifying only width will keep the ratio */
+var width = 854;
+var height = (width * 480) / 854;
+var fullScreenButton;
+
+var game = new Phaser.Game(width, height, Phaser.AUTO, null, {
+  preload: preload, create: create, update: update
+});
+function preload()
+{
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  game.stage.backgroundColor = '#d6e3df';
+  game.load.spritesheet('button', 'img/fullscreen.png', 120, 40);
+}
+
+function gofull()
+{
+  if (game.scale.isFullScreen)
+    game.scale.stopFullScreen();
+  else
+    game.scale.startFullScreen(false);
+}
+
+function create() {
+  var graphics = game.add.graphics(0, 0);
+  var dim = width / 3;
+  var top = (height - dim) / 2;
+  var firstLeft = ((width / 2) - dim) / 2;
+  var secondLeft = firstLeft + (width / 2);
+
+  graphics.lineStyle(2, 0x0000FF, 1);
+  graphics.beginFill(0xFFFF0B, 0.5);
+  graphics.drawRect(firstLeft, top, dim, dim);
+  graphics.drawRect(secondLeft, top, dim, dim);
+  graphics.endFill();
+
+  window.graphics = graphics;
+  game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+  startButton = game.add.button(game.world.width * 0.5, game.world.height * 0.5,
+    'button', gofull, this, 1, 0, 2);
+  startButton.anchor.set(0.5);
+}
+
+function update()
+{
+
+}
 

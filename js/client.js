ca62d4a4eef385dcb4855c5fe9c496d3bab4e469
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-12-15 07:23:00 +0100

    basic multitouch handling

--- js/client.js
+++ js/client.js
@@ -18,6 +18,11 @@ function preload()
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#f2fffb';
   game.load.spritesheet('button', 'img/fullscreen.png', 120, 40);
+  /* disable pinch to zoom */
+  window.ontouchstart = function(e){
+    e.preventDefault();
+    e.stopPropagation();
+  }
 }
 
 function gofull()
@@ -39,8 +44,8 @@ function create()
   startButton = game.add.button(game.world.width * 0.5, game.world.height * 0.5,
     'button', gofull, this, 1, 0, 2);
   startButton.anchor.set(0.5);
-  debugTextLeft = game.add.text(5, 5, "false");
-  debugTextRight = game.add.text(width / 2 + 5, 5, "false");
+  debugTextLeft = game.add.text(5, 5, "plop");
+  debugTextRight = game.add.text(width / 2 + 5, 5, "plop");
 
   controlLeft = new Control(game, width / 4, height / 2, dim);
   controlRight = new Control(game, (3 * width) / 4, height / 2, dim);
@@ -50,16 +55,26 @@ function draw()
 {
   controlLeft.draw();
   controlRight.draw();
-  debugTextLeft.setText(controlLeft.isIn(game.input.x, game.input.y));
-  debugTextRight.setText(controlRight.isIn(game.input.x, game.input.y));
+  var p = game.input.pointer1;
+  debugTextLeft.setText(Math.trunc(p.x) + ", " + Math.trunc(p.y));
+  p = game.input.pointer2;
+  debugTextRight.setText(Math.trunc(p.x) + ", " + Math.trunc(p.y));
+}
+
+function updateControls(pointer)
+{
+  if (controlLeft.isIn(pointer.x, pointer.y)) {
+    controlLeft.set(pointer.x, pointer.y);
+  }
+  if (controlRight.isIn(pointer.x, pointer.y)) {
+    controlRight.set(pointer.x, pointer.y);
+  }
 }
 
 function update()
 {
-  if (game.input.x < width / 2)
-    controlLeft.set(game.input.x, game.input.y);
-  else
-    controlRight.set(game.input.x, game.input.y);
+  updateControls(game.input.pointer1);
+  updateControls(game.input.pointer2);
   draw();
 }
 
